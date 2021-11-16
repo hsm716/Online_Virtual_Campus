9a1b0b5b3c3349040e151d142e63bee4666e5941
hsm716 <hsm716200@naver.com>
2021-11-16 22:34:35 +0900

    .
    .

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
-public class GameManager : MonoBehaviour
+public class GameManager : Photon.MonoBehaviour,IPunObservable
 {
-    // Start is called before the first frame update
+
+    public string[] GamePlayer_list = new string[100];
+
+
+
+    public int time = 3;
+
+
+
+
+    public void JoinGame()
+    {
+        for (int i = 0; i < GamePlayer_list.Length; i++) {
+            if (GamePlayer_list[i] == "빔") {
+                GamePlayer_list[i] = PhotonNetwork.player.NickName;
+                break;
+            }
+        }
+    }
+
+
+
+
     void Start()
     {
-        
+        for(int i = 0; i < 100; i++)
+        {
+            GamePlayer_list[i] = "빔";
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            stream.SendNext(GamePlayer_list);
+            stream.SendNext(time);
+        }
+        else
+        {
+            GamePlayer_list = (string[])stream.ReceiveNext();
+            time = (int)stream.ReceiveNext();
+        }
     }
 }
